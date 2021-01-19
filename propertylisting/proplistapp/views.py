f046e84715288a88518bc6d5f97a8cc03d845004
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2021-01-19 19:14:34 +0500

    :heavy_check_mark: fixed posting an Ad issue

--- propertylisting/proplistapp/views.py
+++ propertylisting/proplistapp/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from proplistapp.forms import PostAdForm
 from proplistapp.models import PostAd
 # Create your views here.
 def index(request):
     if request.method=="POST":
-        form =PostAdForm(request.POST)
+        form =PostAdForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-        return redirect('index') 
+        return redirect('showAds') 
     else:    
         form = PostAdForm()
         return render(request,"index.html",{'postAdData': form})
 
 def showAds(request):
+    if request.method == 'GET': 
         Houses = PostAd.objects.all()
         return render(request, 'housesads.html', {'houses' : Houses})
