85dd030f66746c68177aee5aea08696e3ba1fe0d
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2021-01-22 22:58:18 +0500

    :heavy_check_mark: Login,Logout Authentication added

--- propertylisting/proplistapp/views.py
+++ propertylisting/proplistapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from proplistapp.forms import PostAdForm
-from proplistapp.forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import PostAdForm
+from .forms import RegisterForm
 from proplistapp.models import PostAd
 # Create your views here.
 def index(request):
@@ -20,11 +21,26 @@ def propertyAds(request):
         return render(request, 'PropertyAds.html', {'houses' : Houses})
 
 def register(request):
-    form = CreateUserForm()
+    add_form = RegisterForm()
 #    form = UserCreationForm()
     if request.method == 'POST':
-        if form.is_valid():
-            form.save()
+        if add_form.is_valid():
+            add_form.save()
             return redirect('login')
-    context = {'form':form}
-    return render(request, 'register.html, context)
+    context = {'form':add_form}
+    return render(request, 'register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+    context = {}
+    return render(request, 'login.html', context)    
+
+def logout(request):
+    logout(request)
+    return redirect('login')
